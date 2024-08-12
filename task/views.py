d6ad406142520c3d5d3dbc1686444e9685413cb2
LesterYuman <lesteryuman3@gmail.com>
2024-08-12 12:38:30 -0600

    frist commit

---
+++ task/views.py
@@ -0,0 +1,124 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth  import login, logout,authenticate
+from django.db import IntegrityError
+from .forms import TaskForm, Task
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def signup(request):
+
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+            'form': UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(
+                    username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('task')
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    'error': 'User alredy exist'})
+
+        return render(request, 'signup.html', {
+            'form': UserCreationForm,
+            'error': 'Password do not match'})
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'form': AuthenticationForm,
+        })
+    else:
+        user = authenticate (request, username=request.POST['username'], password= request.POST['password'])
+
+        if user is None:
+            return render(request, 'signin.html', {
+            'form': AuthenticationForm,
+            'error': 'Username or password is incorrect'
+        })
+        else:
+            login(request, user)
+            return redirect('task')
+
+@login_required
+def task(request):
+    tasks = Task.objects.filter(user = request.user, datecompleted__isnull = True)
+
+    return render(request, 'task.html', {
+        'tasks': tasks
+    })
+
+@login_required
+def createTask(request):
+    if request.method == 'GET':
+        return render(request,'create_task.html', {
+        'forms': TaskForm,
+        })
+    else:
+        try:
+            form = TaskForm(request.POST)
+            new_task = form.save(commit=False)
+            new_task.user = request.user
+            new_task.save()
+            return redirect('task')
+        except ValueError:
+            return render(request,'create_task.html', {
+                'forms': TaskForm,
+                'error': 'Please provide valida data'
+                })
+
+@login_required
+def task_detail(request, task_id):
+    if request.method == 'GET':
+        task = get_object_or_404(Task, pk = task_id, user=request.user)
+        form = TaskForm(instance= task)
+        return render(request, 'task_detail.html', {'task': task, 'form': form} )
+    else:
+        try:
+            task = get_object_or_404(Task, pk=task_id, user=request.user)
+            form = TaskForm(request.POST, instance=task)
+            form.save()
+            return redirect('task')
+        except ValueError:
+            return render(request, 'task_detail.html', {'task': task, 'form': form, 'error': 'Error updating task'})
+
+@login_required
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user = request.user)
+    if request.method == 'POST':
+        task.datecompleted = timezone.now()
+        task.save()
+    return redirect('task')
+
+@login_required
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id, user=request.user)
+    if request.method == 'POST':
+        task.delete()
+    return redirect('task')
+
+@login_required
+def task_completed(request):
+    tasks = Task.objects.filter(user= request.user, datecompleted__isnull = False).order_by('-datecompleted')
+    return render(request, 'task.html', {'tasks': tasks})
+
+
+
+
