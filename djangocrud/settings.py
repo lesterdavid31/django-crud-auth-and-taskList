e6cc69432ba022269ab4e87b156af5b84a782504
LesterYuman <lesteryuman3@gmail.com>
2024-08-12 13:07:19 -0600

    modificar sitaxis del la propiedad Middleware, whitenoise

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhitNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'djangocrud.urls'
